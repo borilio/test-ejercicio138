b53220f74d202b8cb79b13acb7881083ef77fd75
borilio <exprimetuandroid@gmail.com>
2023-06-01 12:50:37 +0200

    Añadidas los comentarios de las pruebas unitarias que tenéis que hacer

--- src/test/java/com/inserta/utilidades/DispensadorJabonTest.java
+++ src/test/java/com/inserta/utilidades/DispensadorJabonTest.java
@@ -7,9 +7,7 @@ import static org.junit.Assert.*;
 public class DispensadorJabonTest {
 
     /**
-     * TODO Probamos que al crear un dispensador por defecto, se cumpla lo siguiente...
-     * La prueba unitaria dará correcto si se cumplen las 3 condiciones. No pasará la prueba si
-     * se incumple alguna de las condiciones.
+     * La prueba unitaria dará correcto si se cumplen las 3 condiciones.
      * <ul>
      *     <li>La marca no es null</li>
      *     <li>Está apagado</li>
@@ -18,8 +16,29 @@ public class DispensadorJabonTest {
      */
     @Test
     public void creacionPorDefecto(){
-        fail("No implementado");
+        DispensadorJabon jabonera = new DispensadorJabon();
+        assertNotNull("La marca es null", jabonera.getMarca());
+        assertTrue("La jabonera no está apagada", jabonera.isApagado());
+        assertTrue("La jabonera no está vacía", jabonera.estaVacio());
     }
+
+    /**
+     * Hacer una prueba unitaria para comprobar el dispensador se apaga automáticamente al llegar al 0% de batería
+     */
+
+
+    /**
+     * Hacer una prueba unitaria para comprobar el dispensador se apaga automáticamente al encenderlo muchas veces
+     */
+
+
+    /**
+     * Hacer una prueba unitaria para comprobar el dispensador se apaga automáticamente si se queda sin batería
+     * al dispensar muchas veces.
+     */
+
+
+
 
 
 }
